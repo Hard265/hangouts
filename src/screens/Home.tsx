03bf25ecff04df624d2713154fe31aa64481a64f
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-10-20 08:02:58 +0000

    sync

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex flex-1 justify-center items-center">
+      <Text classNsme="font bold text-xl">Home</Text>
+    </View>
+  )
+}
